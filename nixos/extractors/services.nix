fc66cc9fe7cced450ec51148065f999f7fab4a28
KP64 <karamalsadeh@hotmail.com>
2025-12-28 15:36:27 +0100

    chore(extractors/services): navidrome show baseurl if available

--- nixos/extractors/services.nix
+++ nixos/extractors/services.nix
@@ -327,6 +327,7 @@ in {
       navidrome = mkIf config.services.navidrome.enable {
         name = "Navidrome";
         icon = "services.navidrome";
+        info = mkIf (config.services.navidrome.settings ? BaseUrl) config.services.navidrome.settings.BaseUrl;
         details.listen = mkIf config.services.navidrome.openFirewall {text = "${config.services.navidrome.settings.Address}:${toString config.services.navidrome.settings.Port}";};
       };
 
