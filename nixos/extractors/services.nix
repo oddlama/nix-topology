c4cc521658d573f2021409a4cf273d34b7b89bfe
KP64 <karamalsadeh@hotmail.com>
2026-02-24 17:48:26 +0100

    chore(extractors/services): get rid of obsolescence traces

--- nixos/extractors/services.nix
+++ nixos/extractors/services.nix
@@ -345,11 +345,14 @@ in
         };
       };
 
-      kanidm = mkIf config.services.kanidm.enableServer {
+      kanidm = mkIf (config.services.kanidm.server.enable or config.services.kanidm.enableServer) {
         name = "Kanidm";
         icon = "services.kanidm";
-        info = config.services.kanidm.serverSettings.origin;
-        details.listen.text = config.services.kanidm.serverSettings.bindaddress;
+        info =
+          config.services.kanidm.server.settings.origin or config.services.kanidm.serverSettings.origin;
+        details.listen.text =
+          config.services.kanidm.server.settings.bindaddress
+            or config.services.kanidm.serverSettings.bindaddress;
       };
 
       kavita = mkIf config.services.kavita.enable {
